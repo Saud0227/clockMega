3511901edf0524689d055d98e015dbee237cef31
Saud0227 <saud02tech@gmail.com>
2022-09-20 10:05:00 +0200

    Rtc done

---
+++ clockMega.ino
@@ -0,0 +1,92 @@
+#include <Servo.h>
+#include "U8glib.h"
+#include <Wire.h>
+#include <RtcDS3231.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin, -1 for arduino share
+#define SERVO_PIN 9
+#define ANALOG_READ_PIN 14
+#define OVERRIDE_SNAP 8
+
+
+U8GLIB_SSD1306_128X64 oled(U8G_I2C_OPT_NONE);
+
+RtcDS3231<TwoWire> rtcModule(Wire);
+
+//Servo object
+Servo myServo;
+//Curent angle
+int cPos = 0;
+//Screen Switch
+
+
+byte tH;
+byte tM;
+byte tS;
+
+
+
+void setup(){
+    Serial.begin(9600);
+    myServo.attach(SERVO_PIN);
+    pinMode(ANALOG_READ_PIN, 0);
+    pinMode(OVERRIDE_SNAP, 0);
+    oled.setFont(u8g_font_helvB10);
+
+    rtcModule.Begin();
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    rtcModule.SetDateTime(compiled);
+}
+
+
+void setPos(int inVal, int tolerance=0){
+    //IN: Degree to set servo, tolerance to be the min degree to be set, standard is 0.
+    //OUT: Void function
+    //DO: Check if change exceeds tolerance, if true change motor position and store new position to memory.
+    if (abs(inVal-cPos)>tolerance){
+        myServo.write(inVal);
+        cPos = inVal;
+        Serial.print(inVal);
+        Serial.print("\n");
+    }
+}
+
+
+
+float rtcUpdate(){
+    RtcDateTime now = rtcModule.GetDateTime();
+    RtcTemperature rtcTemp = rtcModule.GetTemperature();
+
+    tH = now.Hour();
+    tM = now.Minute();
+    tS = now.Second();
+    return(rtcTemp.AsFloatDegC());
+
+}
+
+void screenWrite(float cTemp){
+    oled.firstPage();
+    do{
+        //oled.drawStr(0,30, String(cPos).c_str());
+        oled.drawStr(10, 20, String(String(tH) + ":" + String(tM)).c_str());
+        oled.drawStr(10,40,String(cTemp).c_str());
+    }while(oled.nextPage());
+}
+
+
+void loop(){
+    //int inVal = analogRead(ANALOG_READ_PIN);
+    //int degWrite = map(inVal, 0, 1023, 0, 180);
+
+    //setPos(degWrite,15*(abs(digitalRead(OVERRIDE_SNAP)-1)));
+    float rtcTemp = rtcUpdate();
+
+    screenWrite(rtcTemp);
+    delay(10);
+
+}
+
+
